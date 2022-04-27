b666f8a906286fbf9ab42458e9886acae2a22362
Vânia Tavares <vaniatavares@Vanias-MacBook-Pro.local>
2022-04-27 16:44:23 +0100

    banner preparation

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.scss";
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+      Free Standard Shipping on orders over $99.99
+    </div>
+  )
+}
+
+export default Banner
