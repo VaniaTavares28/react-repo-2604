f361bb8abe2456ce4c297541b901dcc8466ea386
VaniaTavares28 <vania.tavares@studiographene.com>
2022-04-28 11:33:49 +0100

    header container

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Header from '../../components/Header';
-import "./style.scss"
+import HeaderContainer from '../../components/layout/containers/HeaderContainer';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <Header/>
-    </div>
+    <>
+    <HeaderContainer>
+      <div>Home</div>
+      </HeaderContainer>
+    </>
   )
 }
 
