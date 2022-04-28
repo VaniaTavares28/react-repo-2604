72ec73e4164d1ce98f9fca43a3aca0ccd91c7c96
VaniaTavares28 <vania.tavares@studiographene.com>
2022-04-28 13:48:59 +0100

    reorganizing images

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import instagram from "./001-instragam-2.png"
+import plant01 from './plant-1.png'
+import plant02 from './plant-2.png'
+import plant03 from './plant-3.png'
+
+export const images = {
+instagram,
+plant01,
+plant02,
+plant03
+}
