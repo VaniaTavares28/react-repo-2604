082383a96acc8c24e608bb886c58eb48159b2c4e
VaniaTavares28 <vania.tavares@studiographene.com>
2022-04-28 12:09:53 +0100

    container readjustments

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../../components/Header';
 import HeaderContainer from '../../components/layout/containers/HeaderContainer';
+import Banner from "../../components/Banner"
+
 const Home = () => {
   return (
     <>
-    <HeaderContainer/>
+    <Banner/>
+    <HeaderContainer>
+      <Header/>
+    </HeaderContainer>
     </>
   )
 }
