b1a32f584db42ea73bdd7cebdabc2e13fd54d33a
VaniaTavares28 <vania.tavares@studiographene.com>
2022-05-04 10:32:32 +0100

    adjustments in class

--- src/components/ColoredSection/index.jsx
+++ src/components/ColoredSection/index.jsx
@@ -4,11 +4,11 @@ import './style.scss';
 
 const ColoredSection = ({title, text, image, reverse, color}) => {
   return (
-    <div className={`colored-section-${color}${reverse ? "-reverse" : null}`}>
+    <div className={`colored-section-${color}${reverse ? "-reverse" : ""}`}>
       <div className='colored-section-content'>
         <h3>{title}</h3>
         <p>{text}</p>
-        <Button text="Shop More" type={color === "silver_rust" ? "brown-no-fill" : "white-no-fill"}/>
+        <Button buttonContent="Shop More" buttonType={color === "silver_rust" ? "brown-no-fill" : "white-no-fill"}/>
       </div>
       <div className='colored-section-image'>
         <img src={image} alt={title} />
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,7 +3,8 @@ export { default as Banner } from "./Banner";
 export { default as Button } from "./Button";
 export { default as Cards } from "./Cards";
 export { default as CardsHeader } from './CardsHeader';
-export { default as Cart } from './Cart'
+export { default as Cart } from './Cart';
+export { default as ColoredSection } from './ColoredSection';
 export { default as Header } from './Header';
 export { default as Highlight } from './HighlightBanner';
 export { default as LandingContent } from './LandingContent';
